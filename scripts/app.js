c7ae0013abc120bda56b7c6c4f5cf67739b9d2c1
Souphan <souphan.ctr@gmail.com>
2016-04-30 11:43:43 -0500

    updated gamershelf

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,38 @@
+//wrap in document.ready 
+jQuery(document).ready(function ($) {
 
-//wrap in document.ready        
+     // Call Gridder
+    $(".gridder").gridderExpander({
+        scrollOffset: 60,
+        scrollTo: "panel", // "panel" or "listitem"
+        animationSpeed: 400,
+        animationEasing: "easeInOutExpo",
+        onStart: function () {
+            console.log("Gridder Inititialized");
+            },
+        onExpanded: function (object) {
+            console.log("Gridder Expanded");
+            $(".carousel").carousel();
+            },
+        onChanged: function (object) {
+            console.log("Gridder Changed");
+            },
+        onClosed: function () {
+            console.log("Gridder Closed");
+                        
+                    }
+                });
+            });
 
-var table = document.getElementsByClassName("table-one");
+(function($) {
+    $(document).ready(function() {
+        $.slidebars();
+    });
+    }) (jQuery);
+
+
+
+/*var table = document.getElementsByClassName("table-one");
 
 for (var i = 0; i < table.length ; i++) {
     table[i];
@@ -9,4 +40,4 @@ for (var i = 0; i < table.length ; i++) {
 
 var goTo = function () {
     location.href = document.getElementById('link_id').value;
-}
+}*/
